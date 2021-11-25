36ba3eac4fb5d832b51f09bb8680ea8e6d4fd20d
victoirearena <victoirearena5@gmail.com>
2021-11-25 13:31:37 +0100

    Adding PDP function

--- src/interpretability/interpretability.py
+++ src/interpretability/interpretability.py
@@ -1,4 +1,5 @@
 from PyALE import ale
+from pdpbox import pdp
 import pandas as pd
 
 
@@ -22,3 +23,23 @@ def plot_ale_effect(estimator, X_train: pd.DataFrame, feature: str, grid_size=20
         include_CI=True,
         C=0.9,
     )
+
+def plot_univariate_pdp(estimator, X_train: pd.DataFrame, feature: str, feature_name):
+    """
+    Plots the PDP isolation plot
+    Args:
+        estimator ([type]): model
+        X_train (pd.DataFrame): dataframe of X training set
+        feature (str): feature for which you want to plot ALE
+        feature_name (str): feature name to appear in the legend
+    """
+    pdp_feature = pdp.pdp_isolate(
+        model=estimator, 
+        dataset=X_train, 
+        model_features=X_train.columns, 
+        feature=feature, 
+        num_grid_points=20
+    )
+
+    fig, _ = pdp.pdp_plot(pdp_isolate_out=pdp_feature, feature_name=feature_name)
+    fig.show()
