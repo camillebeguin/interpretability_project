0115c52223c662041cc8ad5aa8484b313b1118dd
hbrull <you@example.com>
2021-11-25 09:45:25 +0100

    added previously modified files to allow merge

--- src/evaluation/performance_evaluation.py
+++ src/evaluation/performance_evaluation.py
@@ -4,12 +4,21 @@ import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+)
 import constants as cst
 
 logger = logging.getLogger(__name__)
 
-def custom_cross_evaluate(estimator, X: pd.DataFrame, y: pd.Series, cv=3, metric=accuracy_score): 
+
+def custom_cross_evaluate(
+    estimator, X: pd.DataFrame, y: pd.Series, cv=3, metric=accuracy_score
+):
     kfolds = StratifiedKFold(n_splits=cv).split(X, y)
     scores = []
 
@@ -17,25 +26,26 @@ def custom_cross_evaluate(estimator, X: pd.DataFrame, y: pd.Series, cv=3, metric
         X_train, y_train = X.loc[train_idx, :], y.loc[train_idx]
         X_test, y_test = X.loc[test_idx, :], y.loc[test_idx]
 
-        X_train_encoded, X_test_encoded = X_train, X_test # processing function
+        X_train_encoded, X_test_encoded = X_train, X_test  # processing function
         estimator.fit(X_train_encoded, y_train)
         y_pred = estimator.predict(X_test_encoded)
         scores.append(metric(y_test, y_pred))
 
     return scores
 
+
 def compute_performance_metrics(y_true, y_pred):
-    most_common_class_count = np.sum(np.array(y_true)==cst.most_common_class)
-    benchmark_score = most_common_class_count/len(y_true)
+    most_common_class_count = np.sum(np.array(y_true) == cst.most_common_class)
+    benchmark_score = most_common_class_count / len(y_true)
 
     metrics = pd.DataFrame(
         {
             "benchmark": benchmark_score,
-            "accuracy": [accuracy_score(y_true, y_pred)], 
-            "precision": [precision_score(y_true, y_pred)], 
-            "recall": [recall_score(y_true, y_pred)], 
-            "F1 score": [f1_score(y_true, y_pred)], 
-            "ROC AUC score": [roc_auc_score(y_true, y_pred)]
+            "accuracy": [accuracy_score(y_true, y_pred)],
+            "precision": [precision_score(y_true, y_pred)],
+            "recall": [recall_score(y_true, y_pred)],
+            "F1 score": [f1_score(y_true, y_pred)],
+            "ROC AUC score": [roc_auc_score(y_true, y_pred)],
         }
     )
-    return metrics 
+    return metrics
